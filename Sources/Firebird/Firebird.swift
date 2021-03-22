d4cfc4c5769b24296754446527c23bdbd5b8d8c4
Ugo Cottin <ugo.cottin@gmail.com>
2021-03-22 18:57:16 +0100

    making Firebird struc public

--- Sources/Firebird/Firebird.swift
+++ Sources/Firebird/Firebird.swift
@@ -5,11 +5,13 @@
 //  Created by ugo cottin on 20/03/2021.
 //
 
-struct Firebird {
-	var name: String = "Hello, Firebird!"
-	var array: Array<ISC_STATUS> = Array(repeating: 0, count: Int(ISC_STATUS_LENGTH))
+public struct Firebird {
+	public var name: String = "Hello, Firebird!"
+	public var array: Array<ISC_STATUS> = Array(repeating: 0, count: Int(ISC_STATUS_LENGTH))
 	
-	var status: Int32 {
+	public init() { }
+	
+	public var status: Int32 {
 		var array = self.array
 		let code = isc_sqlcode(&array)
 		print("code: \(code)")
