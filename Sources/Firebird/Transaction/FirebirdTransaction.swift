1668c95f67d6374dc43218f6386b7666f9b93830
Ugo Cottin <ugo.cottin@gmail.com>
2021-04-13 18:50:04 +0200

    add isOpened computed property

--- Sources/Firebird/Transaction/FirebirdTransaction.swift
+++ Sources/Firebird/Transaction/FirebirdTransaction.swift
@@ -9,6 +9,11 @@ public class FirebirdTransaction {
 	
 	public var handle: isc_tr_handle
 	
+	/// If the transaction is opened
+	public var isOpened: Bool {
+		self.handle > 0
+	}
+	
 	public init(handle: isc_tr_handle = 0) {
 		self.handle = handle
 	}
