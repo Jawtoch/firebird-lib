4f785d6e12256b0250efc0c03c6f7b0baebcea54
ugocottin <ugo.cottin@gmail.com>
2022-07-11 20:18:00 +0200

    FirebirdConnectionConfiguration documentation

--- Sources/Firebird/Connection/FirebirdConnectionConfiguration.swift
+++ Sources/Firebird/Connection/FirebirdConnectionConfiguration.swift
@@ -1,16 +1,16 @@
-//
-//  FirebirdConnectionConfiguration.swift
-//  
-//
-//  Created by ugo cottin on 24/06/2022.
-//
-
+/// Define parameters of a `FirebirdConnection`
 public struct FirebirdConnectionConfiguration {
 	
+	/// Target of a `FirebirdConnection`
 	public enum Target {
+		
+		/// Connection to a local database file
 		case local(path: String)
+		
+		/// Connection to a remote database server
 		case remote(hostName: String, port: UInt16, path: String)
 		
+		/// String url provided to `isc_attach_database`
 		var attachUrl: String {
 			switch self {
 				case .local(let path):
@@ -21,12 +21,20 @@ public struct FirebirdConnectionConfiguration {
 		}
 	}
 	
+	/// IANA assigned port to GDS-DB service
+	/// See: https://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.xhtml?search=GDS
 	public static var ianaPort: UInt16 { 3050 }
 	
+	/// Connection target
 	public let target: Target
 	
+	/// Connection parameters
 	public var parameters: [FirebirdConnectionParameter]
-		
+	
+	/// Hold configuration for creating a connection
+	/// - Parameters:
+	///   - target: connection target
+	///   - parameters: connection parameters
 	public init(
 		target: Target,
 		parameters: [FirebirdConnectionParameter] = []) {
